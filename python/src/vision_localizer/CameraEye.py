809a379c5ed6e354deab2bfcd885c88d6a001940
amir <amir.h.bakhtiary@gmail.com>
2021-09-29 18:08:38 +0200

    clean up the cameraEye

--- python/src/vision_localizer/CameraEye.py
+++ python/src/vision_localizer/CameraEye.py
@@ -1,29 +1,5 @@
-import cv2
-
-from Capability import Capability, C299_linux_Spec_Factory, C299_win_Spec_Factory
-
-
-class VideoSource:
-
-    def __init__(self, cameraSpecFactory):
-        self.camera_spec_factory = cameraSpecFactory
-
-    def get_source(self):
-        vid = cv2.VideoCapture("/dev/video0")
-        if not vid.isOpened():
-            raise Exception("Error could not connect to camera")
-
-        capability_map = {}
-
-        all_caps = self.camera_spec_factory.get_cap_specs()
-
-        for c in all_caps:
-            capability_id = cv2.__dict__[c.name]
-            vid.set(capability_id, c.val)
-            if c.key is not None:
-                capability_map[c.key] = Capability(capability_id, c.val, c.inc, c.name)
-
-        return vid, capability_map
+from Capability import C299_linux_Spec_Factory, C299_win_Spec_Factory
+from vision_localizer.Capability import VideoSource
 
 
 class CameraEye:
@@ -34,18 +10,16 @@ class CameraEye:
         self.dist_coef = None
         self.direction = +1
 
-
     def calibrate(self, calibration_matrix):
         self.calibration_matrix = calibration_matrix
 
     @classmethod
     def c29_webcam_linux_eye(cls):
-        return CameraEye(VideoSource(C299_linux_Spec_Factory))
+        return CameraEye(C299_linux_Spec_Factory())
 
     @classmethod
     def c29_webcam_win_eye(cls):
-        return CameraEye(VideoSource(C299_win_Spec_Factory))
-
+        return CameraEye(C299_win_Spec_Factory())
 
     def open_eyes(self):
         self.vid, self.capability_map = self.video_source.get_source()
@@ -69,7 +43,7 @@ class CameraEye:
         print(the_char)
         if the_char == '=':
             self.direction = +1
-        if the_char == '+':
+        elif the_char == '+':
                 self.direction = +10
         elif the_char == '-':
             self.direction = -1
--- python/src/vision_localizer/Capability.py
+++ python/src/vision_localizer/Capability.py
@@ -1,6 +1,8 @@
 import dataclasses
 from dataclasses import dataclass
 
+import cv2
+
 
 @dataclass
 class Capability:
@@ -9,7 +11,7 @@ class Capability:
     print_name: str
 
 
-@dataclasses.dataclass
+@dataclass
 class CapSpec:
     name: str
     initial_val: float
@@ -17,8 +19,38 @@ class CapSpec:
     assigned_key: str = None
 
 
-class C299_linux_Spec_Factory:
-    def get_src
+class VideoSource:
+
+    def get_vid(self):
+        raise NotImplementedError()
+
+    def get_cap_specs(self):
+        raise NotImplementedError()
+
+    def get_source(self):
+        vid = self.get_vid()
+
+        capability_map = {}
+
+        all_caps = self.get_cap_specs()
+
+        for c in all_caps:
+            capability_id = cv2.__dict__[c.name]
+            vid.set(capability_id, c.initial_val)
+            if c.assigned_key is not None:
+                capability_map[c.assigned_key] = Capability(capability_id, c.increment, c.name)
+
+        return vid, capability_map
+
+
+class C299_linux_Spec_Factory(VideoSource):
+    def get_vid(self):
+        vid = cv2.VideoCapture("/dev/video0")
+        if not vid.isOpened():
+            raise Exception("Error could not connect to camera")
+
+        return vid
+
     def get_cap_specs(self):
         return [
             CapSpec("CAP_PROP_FRAME_WIDTH", 1920),
@@ -36,7 +68,15 @@ class C299_linux_Spec_Factory:
             CapSpec("CAP_PROP_PAN", 10, 1, 'p'),
         ]
 
-class C299_win_Spec_Factory:
+
+class C299_win_Spec_Factory(VideoSource):
+    def get_vid(self):
+        vid = cv2.VideoCapture(0)
+        if not vid.isOpened():
+            raise Exception("Error could not connect to camera")
+
+        return vid
+
     def get_cap_specs(self):
         return [
             CapSpec("CAP_PROP_FRAME_WIDTH", 1920),
@@ -52,4 +92,24 @@ class C299_win_Spec_Factory:
             CapSpec("CAP_PROP_SHARPNESS", 128, 8, 's'),
             CapSpec("CAP_PROP_GAIN", 255),
             CapSpec("CAP_PROP_PAN", 10, 1, 'p'),
+        ]
+
+
+class Cheap_Cam_Linux_Spec_Factory():
+    def get_vid(self):
+        vid = cv2.VideoCapture("/dev/video1")
+        if not vid.isOpened():
+            raise Exception("Error could not connect to camera")
+
+        return vid
+
+    def get_cap_specs(self):
+        return [
+            CapSpec("CAP_PROP_CONTRAST", 128, 8, 'c'),
+            CapSpec("CAP_PROP_BRIGHTNESS", 128, 8, 'b'),
+            CapSpec("CAP_PROP_SATURATION", 128, 8, 's'),
+            CapSpec("CAP_PROP_SHARPNESS", 128, 8, 's'),
+            # CapSpec("CAP_PROP_HUE", 0, 1, 'h'),
+            # CapSpec("CAP_PROP_GAMMA", 4, 1, 'g'),
+            CapSpec("CAP_PROP_BACKLIGHT", 4, 1, 'l'),
         ]
