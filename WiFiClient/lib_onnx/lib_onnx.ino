b710428255e03a249e1480928a60df5bbe76c635
Amir Hossein Bakhtiary <amir.h.bakhtiary@gmail.com>
2021-11-10 23:05:35 +0100

    nn for esp32

--- WiFiClient/lib_onnx/lib_onnx.ino
+++ WiFiClient/lib_onnx/lib_onnx.ino
@@ -1,64 +1,68 @@
 #include "robotics_config.h"
 
-class Stage{
+struct Stage{
 };
 
 struct Params {
+
     int class_id;  
     int len_of_data;
- //   float * param_data;
+    float * param_data;
 };
 
-Params w0(0, 4);
+Params w0 = {0, 4, ((float []) {1,2,3,4,5,6}) };
+Params b0 = {0, 4, ((float []) {1,2,3}) };
 
-//class FullyConnected: public Stage{
-//  int input_size;
-//  int output_size;
-//  
-//  float * weights;
-//  
-//  
-//};
-//
-//template <int input_size>
-//class Relu: public Stage{
-//  float [input_size] bias;
-//};
-//
-//template <int input_size>
-//class Relu: public Stage{
-//  float [input_size] bias;
-//};
-//
-//template <int input_size>
-//class Tanh: public Stage{
-//  float [input_size];
-//};
-//
-//
-//class NeuralNetwork{
-//  Stage * stages;
-//  int number_of_stages;
-//  public:
-//  NeuralNetwork(Stage * my_stages, int number_of_stages_)
-//  {
-//    stages = my_stages;
-//    number_of_stages = number_of_stages_;
-//  }
-//};
-//
-//FullyConnected<3,4>;
-//Relu()
-//FullyConnected<4,2>;
-//Stage stages []={fc};
-//
-//NeuralNetwork nn(stages, sizeof(stages));
+Params w1 = {0, 4, ((float []) {1,2,3,4,5,6}) };
+Params b1 = {0, 4, ((float []) {1,2}) };
+
+
+struct FullyConnected: public Stage
+{
+  int input_size;
+  int output_size;
+  Params & weights;
+  Params & biases;
+
+  FullyConnected(
+    int input_size,
+    int output_size,
+    Params & weights,
+    Params & biases
+  ): input_size(input_size), output_size(output_size), weights(weights), biases(biases){}
+  
+};
+
+class Relu: public Stage{
+
+};
+
+class Tanh: public Stage{
+
+};
+
+
+class NeuralNetwork{
+  public:
+  Stage * stages;
+  int len_stages;
+};
+
+FullyConnected fc1{2, 3, w0, b0};
+Relu relu1;
+FullyConnected fc2{3, 2, w1, b1};
+Tanh ta;
+Stage stages [] = {fc1, relu1, fc2, ta};
+
+
+NeuralNetwork nn = {stages, sizeof(stages)};
 
 
 void setup() {
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  nn.compute({1.0,1.0});
 }
